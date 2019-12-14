c5311307f3bf7a0d70b8263da30c42f7d57671e1
salehudeen <49670748+salehudeen@users.noreply.github.com>
2019-12-14 09:56:48 +0300

    changes

--- php_stock/php_stockdb.php
+++ php_stock/php_stockdb.php
@@ -20,8 +20,8 @@ class cphp_stock_db {
 	// Database connection info
 	var $Host = 'localhost';
 	var $Port = 3306;
-	var $Username = 'root';
-	var $Password = 'Elvis56';
+	var $Username = 'deen';
+	var $Password = 'deendraco123';
 	var $DbName = 'php_stock';
 
 	// ADODB (Access/SQL Server)
